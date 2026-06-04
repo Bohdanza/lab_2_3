c515f7ef390fcee0148bb8d134ec3188c8a1fcfd
Bohdanza <81554970+Bohdanza@users.noreply.github.com>
2026-06-04 12:05:00 +0300

    rm cached

--- out/build/Debug/_deps/flac-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/uni/lab_2_3/lab_2_3/out/build/Debug/_deps/flac-src
-# Build directory: D:/uni/lab_2_3/lab_2_3/out/build/Debug/_deps/flac-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("microbench")
--- out/build/Debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/uni/lab_2_3/lab_2_3/out/build/Debug/_deps/flac-src/src/libFLAC
-# Build directory: D:/uni/lab_2_3/lab_2_3/out/build/Debug/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
